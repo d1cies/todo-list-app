7c1f6db2be12beda3fd259ed5cf11f741ed778d5
d1cies <vadimschaforostov@ya.ru>
2024-06-20 02:48:50 +0300

    feat(router) with auto_route

---
+++ lib/router/app_router.dart
@@ -0,0 +1,15 @@
+import 'app_router_export.dart';
+
+part 'app_router.gr.dart';
+
+@AutoRouterConfig(replaceInRouteName: 'ScreenWidget,Route')
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(
+          page: TodoListRoute.page,
+          initial: true,
+        ),
+        AutoRoute(page: TodoDetailRoute.page),
+      ];
+}
